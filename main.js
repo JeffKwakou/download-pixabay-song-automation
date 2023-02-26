0c70b29fda7bb085025127ad10ed474cdfaabf6f
Jeff Gbeho <jeff.gbeho@gmail.com>
2023-02-26 22:00:56 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer');
+const download = require('download');
+const delay = require('delay');
+
+const url = 'https://pixabay.com/fr/music/search/lofi%20hip%20hop/';
+const downloadPath = './download';
+const classSelector = 'a.download.audio-download';
+
+async function downloadFiles() {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    for (let i = 0; i < 15; i++) {
+        await page.evaluate(() => {
+            window.scrollBy(0, window.innerHeight);
+        });
+        await delay(1000);
+    }
+
+    try {
+        const elements = await page.evaluate(() => {
+            const audioLink = document.querySelectorAll(classSelector);
+            const urls = Array.from(audioLink).map(v => v.href);
+            return urls;
+        });
+    
+        for (const element of elements) {
+            const response = await download(element, downloadPath);
+            console.log(`File downloaded: ${element}`);
+            await delay(1000);
+        }
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+downloadFiles();
